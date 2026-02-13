bcb6bd556fdb839c21aa42463eb4aea37d072871
onhangocnhu <nhu.onhangoc@hcmut.edu.vn>
2026-02-14 00:20:38 +0700

    feat: add react router

--- petcareclient/src/main.tsx
+++ petcareclient/src/main.tsx
@@ -1,11 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Route, Routes, BrowserRouter } from "react-router";
 
 import "./index.css";
 import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 );
